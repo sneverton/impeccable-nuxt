1db2c70ab049d8530c5d2ca8823e78a59f40f0a4
Everton Andrade <everton@andrade.mat.br>
2026-04-08 07:52:15 -0400

    chore

--- public/js/demos/index.js
+++
@@ -1,6 +0,0 @@
-// Main demos registry - exports all demos
-export { commandDemos, getCommandDemo } from "./commands/index.js";
-export { getSkillDemo, skillDemos } from "./skills/index.js";
-
-
-
--- scripts/lib/transformers/index.js
+++
@@ -1,7 +0,0 @@
-import { createTransformer } from './factory.js';
-import { PROVIDERS } from './providers.js';
-
-export const transformClaudeCode = createTransformer(PROVIDERS['claude-code']);
-export const transformCodex = createTransformer(PROVIDERS.codex);
-
-export { createTransformer, PROVIDERS };
